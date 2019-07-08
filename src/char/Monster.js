16804b5ca09dc4f8b2c3744e71afd9997762a728
Manuel Urbano Santos <mu@member.fsf.org>
2019-07-08 23:22:48 +0200

    some things for monster race

--- src/char/Monster.js
+++ src/char/Monster.js
@@ -1,3 +1,21 @@
 class Monster {
-    
+    constructor(name, race, tier, powers=[]) {
+        this.name = name;
+        this.race = race;
+        this.tier = tier;
+        this.powers = powers;
+
+    }
+
+    addPower(power) {
+        this.powers.push(power);
+        power.apply(this);
+    }
+
+    addPowers(powers) {
+        this.powers = this.powers.concat(powers);
+        for (let p of powers) {
+            p.apply(this);
+        }
+    }
 }
